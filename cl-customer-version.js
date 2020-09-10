f1bde5f2b1ae987a7c5f6f8a88e7f6e9120757eb
hansfpc <hansfpc@outlook.com>
2020-09-10 15:24:00 -0300

    first releas

---
+++ cl-customer-version.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const semver = require('semver')
+const fileName = './packages/apps/cl-customer-webapp/package.json'
+let file = require(fileName)
+
+// console.log(process.argv)
+let currentVersion = file.version;
+let newVersion = semver.inc(file.version, 'patch');
+
+
+console.log('CL-CUSTOMER current version: ', currentVersion)
+console.log('CL-CUSTOMER new version: ', newVersion)
+
+file.version = newVersion
+
+
+fs.writeFile(fileName, JSON.stringify(file, null, 2), function writeJSON(err) {
+    if (err) return console.log(err);
+    console.log('writing to ' + fileName);
+    console.log('UPGRADED PACKAGE VERSION SUCCESSFULLY!')
+});
+
--- scripts/cl-customer-version.js
+++
@@ -1,2 +0,0 @@
-import CLC_PACKAGE_JSON from '../packages/apps/cl-customer-webapp/package.json'
-console.log(CLC_PACKAGE_JSON)
